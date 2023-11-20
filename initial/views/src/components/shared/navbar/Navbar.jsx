8f0e4a2e399aa9bfc824e2c660a334d5a49c192e
mehrani <mehraniali8002@gmail.com>
2023-11-20 14:10:58 +0330

    update navbar

--- initial/views/src/components/shared/navbar/Navbar.jsx
+++ initial/views/src/components/shared/navbar/Navbar.jsx
@@ -1,8 +1,14 @@
 import Logo from "../../../assets/logo.svg";
-import { BsMoonFill, BsSunFill, BsQuestionCircle } from "react-icons/bs";
+import {
+  BsMoonFill,
+  BsSunFill,
+  BsQuestionCircle,
+  BsList,
+} from "react-icons/bs";
+import { CiMenuBurger } from "react-icons/ci";
 import { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { Button } from "react-bootstrap";
+import { Button, Form } from "react-bootstrap";
 
 export default function Navbar() {
   const [theme, setTheme] = useState(getCurrentTheme());
@@ -41,7 +47,14 @@ export default function Navbar() {
   return (
     <nav className="navbar bg-1 justify-content-between">
       <div className="container-fluid navbar-container">
-        <Link className="navbar-brand d-flex align-items-center" to="/">
+        <div className="col-2 d-sm-flex d-md-none justify-content-center">
+          sidebar icon
+        </div>
+
+        <Link
+          className="col-8 col-md-2 d-flex justify-content-center align-items-center text-center"
+          to="/"
+        >
           <img
             src={Logo}
             alt="logo"
@@ -49,9 +62,36 @@ export default function Navbar() {
             height="45"
             className="d-inline-block align-text-top"
           />
-          <span className="text-1 pe-1">App name</span>
+          <span className="text-1 pe-1 d-none d-md-block">App name</span>
         </Link>
-        <div className="d-flex align-items-center">
+
+        <div className="col-8 d-none d-md-block">search form</div>
+
+        <div className="col-2 d-sm-flex d-md-none justify-content-center">
+          search icon
+        </div>
+
+        <div className="col-2 d-md-flex d-none">util</div>
+        {/* <Link className="col-2 d-flex align-items-center d-sm-none" to="/">
+          salam
+        </Link>
+
+        <Link className="col-2 navbar-brand d-flex align-items-center" to="/">
+          <img
+            src={Logo}
+            alt="logo"
+            width="45"
+            height="45"
+            className="d-inline-block align-text-top"
+          />
+          <span className="text-1 pe-1 d-none d-sm-block">App name</span>
+        </Link>
+
+        <div className="col-8 col-sm-4 d-none d-sm-block">
+          <Form.Control size="lg" type="text" placeholder="Large text" />
+        </div>
+
+        <div className="col-sm-2 align-items-center justify-content-end d-none d-sm-flex">
           <Link to="/faq">
             <Button
               className="btn btn-no-bs theme-toggle-btn text-1"
@@ -71,6 +111,16 @@ export default function Navbar() {
             {theme === "dark" && <BsSunFill className="icon-24" />}
           </Button>
         </div>
+
+        <div className="d-flex col-sm-2 align-items-center justify-content-end d-block d-sm-none">
+          <Button
+            className="btn btn-no-bs theme-toggle-btn text-1 pe-0 ps-0"
+            variant=""
+          >
+            <CiMenuBurger className="icon-24" />
+          </Button>
+        </div>
+       */}
       </div>
     </nav>
   );
